deedc07b1afb90d6589f66e3af24a8d98a8c2d72
Narendra Mahara <narendramahara9868@gmail.com>
2025-06-21 15:50:56 +0545

    learn whileInView

---
+++ whileInView/src/App.jsx
@@ -0,0 +1,108 @@
+import { motion } from "motion/react";
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 50,
+        padding: 50,
+      }}
+    >
+      <motion.div
+        style={{
+          padding: 50,
+          fontSize: 50,
+        }}
+        whileInView={{ opacity: 1, rotate: 5, scale: 1.1 }}
+        transition={{
+          duration:1,
+          delay:0.3
+        }}
+      >
+        Hello World
+      </motion.div>
+      <motion.div
+        style={{
+          padding: 50,
+          fontSize: 50,
+        }}
+        whileInView={{ opacity: 1, rotate: 5, scale: 1.1 }}
+        transition={{
+          duration:1,
+          delay:0.3
+        }}
+      >
+        Hello World
+      </motion.div>
+      <motion.div
+        style={{
+          padding: 50,
+          fontSize: 50,
+        }}
+        whileInView={{ opacity: 1, rotate: 5, scale: 1.1 }}
+        transition={{
+          duration:1,
+          delay:0.3
+        }}
+      >
+        Hello World
+      </motion.div>
+      <motion.div
+        style={{
+          padding: 50,
+          fontSize: 50,
+        }}
+        whileInView={{ opacity: 1, rotate: 5, scale: 1.1 }}
+        transition={{
+          duration:1,
+          delay:0.3
+        }}
+      >
+        Hello World
+      </motion.div>
+      <motion.div
+        style={{
+          padding: 50,
+          fontSize: 50,
+        }}
+        whileInView={{ opacity: 1, rotate: 5, scale: 1.1 }}
+        transition={{
+          duration:1,
+          delay:0.3
+        }}
+      >
+        Hello World
+      </motion.div>
+      <motion.div
+        style={{
+          padding: 50,
+          fontSize: 50,
+        }}
+        whileInView={{ opacity: 1, rotate: 5, scale: 1.1 }}
+        transition={{
+          duration:1,
+          delay:0.3
+        }}
+      >
+        Hello World
+      </motion.div>
+      <motion.div
+        style={{
+          padding: 50,
+          fontSize: 50,
+        }}
+        whileInView={{ opacity: 1, rotate: 5, scale: 1.1 }}
+        transition={{
+          duration:1,
+          delay:0.3
+        }}
+      >
+        Hello World
+      </motion.div>
+      
+    </div>
+  );
+}
+
+export default App;
